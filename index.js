d7fe60b41c9a61ce4d3bb382c345a9e934fb2fa7
Danosnaya <daniel.osnaya@sistemashumanos.com>
2021-07-05 10:20:42 -0500

    Vulnerability active

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.get('/', function (req, res) {
     res.send('Pipeline numero 162 correcto')
 })
 
+app.get('/dashboard', function (req, res) {
+    res.send('hello world')
+})
+app.get('/dashboard', function (req, res) {
+    res.send('hello world 2')
+})
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
